69229228ce21715d1da524ccbb68aa7a392a4cae
matt-in-a-hat <matt@mediasuite.co.nz>
2016-12-08 14:03:14 +0100

    Add more content

--- open.php
+++ open.php
@@ -1,14 +1,11 @@
 <?php
-
 // error_reporting(E_ALL);
 // ini_set('display_errors', 1);
-
 function respond($message, $status=200) {
   http_response_code($status);
   echo $message;
   exit();
 }
-
 $messages = array(
   'A cat dies due to your curiosity.',
   'I love the way you love lame things I make... like this ;)',
@@ -18,6 +15,12 @@ $messages = array(
   'Flambo is red, Ice king is blue, Marcy is cute, but not cuter than you.',
   '<p>I love that we want to travel the world together.</p><a href="https://www.facebook.com/photo.php?fbid=1459148847444563&set=a.554924301200360.150033.100000483843161&type=3&theater"><img src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/13428011_1459148847444563_2827824956562572338_n.jpg?oh=569ff755893560b68ddd75cb4ab49558&oe=58C7870F" /><br>(see original)</a>',
   '<p>I love how you get along with my weird family.</p><a href="https://www.facebook.com/photo.php?fbid=10205272671847099&set=a.10205114948384111.1073741900.1556160189&type=3&theater"><img src="https://scontent-arn2-1.xx.fbcdn.net/t31.0-8/10926339_10205272671847099_3339682693110442607_o.jpg" /><br>(see original)</a>',
+  'I love your cuddles, and that you don\'t get sick of mine.',
+  '<p>I love that we both love floofy things.</p><img src="http://static.boredpanda.com/blog/wp-content/uploads/2016/03/hairy-fluffy-cat-sky-the-ragdoll-latest.jpg" />',
+  'I love we share many opinions, and are empathetic with opinions we don\'t.',
+  '<p>I love the things we get up to.</p><iframe width="560" height="315" src="https://www.youtube.com/embed/A6Nf1-BvYAY" frameborder="0" allowfullscreen></iframe><p>PS happy birthday <3</p>',
+  'I love your sense of humour.',
+  'I love your cute hands, and I love to hold them.',
 );
 
 // IDEAS
---
+++ pies.md
@@ -0,0 +1,17 @@
+Maddy,
+why did we bake those pies?
+We could have bought them,
+and probably saved some time.
+But we made them,
+yeah we made those pies.
+You almost cried,
+but I didn't see you cry.
+
+Maddy,
+do you even like them?
+I guess someone liked it,
+for the best taste award bit.
+What kind of person makes a hundred pies?
+And it doesn't even break their stride.
+Maddy there were fears there,
+if we failed would you even care?
